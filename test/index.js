6ccc4eeabc77c76cf4413865b54c525b75b2eff0
cdaringe <cdieringer@mrn.org>
2015-02-13 09:23:29 +0100

    3.0.0 rc

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ var viewConventions = require('ampersand-view-conventions');
 var SelectView = require('../ampersand-select-view');
 var AmpersandState = require('ampersand-state');
 var AmpersandCollection = require('ampersand-collection');
+var dom = require('ampersand-dom');
 
 var Model = AmpersandState.extend({
     props: {
@@ -13,8 +14,20 @@ var Model = AmpersandState.extend({
     }
 });
 
+var VolatileModel = AmpersandState.extend({
+    props: {
+        id: 'any', // lookout!,
+        title: 'string',
+        disabled: 'boolean'
+    }
+});
+
 var Collection = AmpersandCollection.extend({
     model: Model
+});
+
+var VolatileCollection = AmpersandCollection.extend({
+    model: VolatileModel
 });
 
 if (!Function.prototype.bind) Function.prototype.bind = require('function-bind');
@@ -23,6 +36,9 @@ var fieldOptions = {
     name: 'word',
     options: ['foo', 'bar', 'baz']
 };
+var view;
+var arr, arrNum;
+var coll;
 
 viewConventions.view(suite.tape, SelectView, fieldOptions);
 viewConventions.formField(suite.tape, SelectView, fieldOptions, 'foo');
@@ -35,16 +51,14 @@ var sync = function (cb) {
     };
 };
 
-suite('Setup', function (s) {
-    var view;
 
+suite('Setup', function (s) {
     s.beforeEach(function () {
-        var fieldOptions = {
+        fieldOptions = {
             name: 'word',
             label: 'Choose a word',
             options: ['foo', 'bar', 'baz']
         };
-
         view = new SelectView(fieldOptions);
     });
 
@@ -77,13 +91,53 @@ suite('Setup', function (s) {
         var selectName = view.el.querySelector('select').getAttribute('name');
         t.equal(selectName, 'word');
     }));
+
+    s.test('eagerValidation', sync(function (t) {
+        view = new SelectView({
+            name: 'num',
+            options: fieldOptions.options,
+            eagerValidate: true,
+            required: true,
+            unselectedText: 'default'
+        });
+
+        t.ok(dom.hasClass(view.el, view.invalidClass), 'validation message should be present on el with eagerValidate');
+    }));
+
+});
+
+suite('Options array with number items', function (s) {
+    s.beforeEach(function () {
+        arr = [0, 1, 1.5, 2];
+        view = null;
+    });
+
+    s.test('renders the number options into the select', sync(function (t) {
+        view = new SelectView({ name: 'num', options: arr });
+
+        var optionNodes = view.el.querySelectorAll('select option');
+
+        t.equal(optionNodes.length, 4);
+
+        t.equal(optionNodes[0].value, '0');
+        t.equal(optionNodes[0].textContent, '0');
+
+        t.equal(optionNodes[1].value, '1');
+        t.equal(optionNodes[1].textContent, '1');
+
+        t.equal(optionNodes[2].value, '1.5');
+        t.equal(optionNodes[2].textContent, '1.5');
+    }));
+
 });
 
 suite('Options array with string items', function (s) {
-    var arr = ['one', 'two', 'three'];
-    var view;
+    s.beforeEach(function () {
+        arr = ['one', 'two', 'three'];
+        view = null;
+    });
 
-    s.test('renders the options into the select', sync(function (t) {
+    s.test('renders the options into the select (array)', sync(function (t) {
         view = new SelectView({ name: 'word', options: arr });
 
         var optionNodes = view.el.querySelectorAll('select option');
@@ -118,7 +172,7 @@ suite('Options array with string items', function (s) {
         t.equal(optionNodes[1].textContent, 'one');
     }));
 
-    s.test('selects the right item', sync(function (t) {
+    s.test('selects the right item (options: [\'valAndText\'])', sync(function (t) {
         view = new SelectView({ name: 'word', options: arr, unselectedText: 'Please choose:', value: 'two' });
 
         var select = view.el.querySelector('select');
@@ -131,25 +185,50 @@ suite('Options array with string items', function (s) {
         view.setValue('one');
         t.equal(select.options[select.selectedIndex].value, 'one');
 
-        view.setValue('totes-wrong');
-        t.equal(select.options[select.selectedIndex].innerHTML, 'Please choose:');
-
+        try {
+            view.setValue('invalid-option');
+            t.ok(false, 'unable to set invalid option');
+        } catch (err) {
+            t.ok(true, 'unable to set invalid option');
+        }
     }));
 
     s.test('options are enabled', sync(function (t) {
+        view = new SelectView({ name: 'word', options: arr, unselectedText: 'Please choose:', value: 'two' });
         var optionNodes = view.el.querySelectorAll('select option');
-
         t.equal(optionNodes[0].disabled, false);
         t.equal(optionNodes[1].disabled, false);
         t.equal(optionNodes[2].disabled, false);
     }));
+
 });
 
 suite('Options array with array items', function (s) {
-    var arr =  [ ['one', 'Option One'], ['two', 'Option Two', false], ['three', 'Option Three', true] ];
-    var view;
+    s.beforeEach(function() {
+        arr =  [ ['one', 'Option One'], ['two', 'Option Two', false], ['three', 'Option Three', true] ];
+        arrNum =  [ [0, 'Option Zero'], [1, 1, false], [1.5, 1.5, true] ];
+        view = null;
+    });
 
-    s.test('renders the options into the select', sync(function (t) {
+    s.test('renders the arr-num options into the select', sync(function (t) {
+        view = new SelectView({ name: 'num', options: arrNum });
+
+        var optionNodes = view.el.querySelectorAll('select option');
+
+        t.equal(optionNodes.length, 3);
+
+        t.equal(optionNodes[0].value, '0');
+        t.equal(optionNodes[0].textContent, 'Option Zero');
+
+        t.equal(optionNodes[1].value, '1');
+        t.equal(optionNodes[1].textContent, '1');
+
+        t.equal(optionNodes[2].value, '1.5');
+        t.equal(optionNodes[2].textContent, '1.5');
+        t.ok(optionNodes[2].disabled, true);
+    }));
+
+    s.test('renders the arr-str options into the select', sync(function (t) {
         view = new SelectView({ name: 'word', options: arr });
 
         var optionNodes = view.el.querySelectorAll('select option');
@@ -184,7 +263,7 @@ suite('Options array with array items', function (s) {
         t.equal(optionNodes[1].textContent, 'Option One');
     }));
 
-    s.test('selects the right item', sync(function (t) {
+    s.test('selects the right item (options:  [[\'val\', \'text\']])', sync(function (t) {
         view = new SelectView({ name: 'word', options: arr, unselectedText: 'Please choose:', value: 'two' });
 
         var select = view.el.querySelector('select');
@@ -197,8 +276,12 @@ suite('Options array with array items', function (s) {
         view.setValue('one');
         t.equal(select.options[select.selectedIndex].value, 'one');
 
-        view.setValue('totes-wrong');
-        t.equal(select.options[select.selectedIndex].innerHTML, 'Please choose:');
+        try {
+            view.setValue('totes-wrong');
+            t.ok(false, 'unable to set invalid option');
+        } catch(err) {
+            t.ok('unable to set invalid option');
+        }
     }));
 
     s.test('renders a disabled item if a third value is passed which is truthy', sync(function (t) {
@@ -213,14 +296,17 @@ suite('Options array with array items', function (s) {
 });
 
 suite('With ampersand collection', function (s) {
-    var coll = new Collection([
-        { id: 1, someOtherKey: 'foo', title: 'Option one' },
-        { id: 2, someOtherKey: 'bar', title: 'Option two' },
-        { id: 3, someOtherKey: 'baz', title: 'Option three' }
-    ]);
-    var view;
+    s.beforeEach(function() {
+        view = null;
+        coll = new Collection([
+            { id: 0, someOtherKey: 'zero', title: 'Option zero' },
+            { id: 1, someOtherKey: 'foo',  title: 'Option one' },
+            { id: 2, someOtherKey: 'bar',  title: 'Option two' },
+            { id: 3, someOtherKey: 'baz',  title: 'Option three' }
+        ]);
+    });
 
-    s.test('renders the options into the select', sync(function (t) {
+    s.test('renders the options into the select (collection)', sync(function (t) {
         view = new SelectView({
             name: 'word',
             options: coll,
@@ -229,17 +315,19 @@ suite('With ampersand collection', function (s) {
         });
 
         var optionNodes = view.el.querySelectorAll('select option');
+        t.equal(optionNodes.length, 4, 'node list length (collection)');
 
-        t.equal(optionNodes.length, 3);
+        t.equal(optionNodes[0].value, '0');
+        t.equal(optionNodes[0].textContent, 'Option zero');
 
-        t.equal(optionNodes[0].value, '1');
-        t.equal(optionNodes[0].textContent, 'Option one');
+        t.equal(optionNodes[1].value, '1');
+        t.equal(optionNodes[1].textContent, 'Option one');
 
-        t.equal(optionNodes[1].value, '2');
-        t.equal(optionNodes[1].textContent, 'Option two');
+        t.equal(optionNodes[2].value, '2');
+        t.equal(optionNodes[2].textContent, 'Option two');
 
-        t.equal(optionNodes[2].value, '3');
-        t.equal(optionNodes[2].textContent, 'Option three');
+        t.equal(optionNodes[3].value, '3');
+        t.equal(optionNodes[3].textContent, 'Option three');
     }));
 
     s.test('rerenders if the collection changes', sync(function (t) {
@@ -286,7 +374,7 @@ suite('With ampersand collection', function (s) {
         t.equal(optionNodes[1].textContent, 'Option twenty');
     }));
 
-    s.test('renders the options into the select with different id attribute', sync(function (t) {
+    s.test('renders the options into the select with different id attribute (collection)', sync(function (t) {
         view = new SelectView({
             name: 'word',
             options: coll,
@@ -296,11 +384,12 @@ suite('With ampersand collection', function (s) {
 
         var optionNodes = view.el.querySelectorAll('select option');
 
-        t.equal(optionNodes.length, 3);
+        t.equal(optionNodes.length, 4);
 
-        t.equal(optionNodes[0].value, 'foo');
-        t.equal(optionNodes[1].value, 'bar');
-        t.equal(optionNodes[2].value, 'baz');
+        t.equal(optionNodes[0].value, 'zero');
+        t.equal(optionNodes[1].value, 'foo');
+        t.equal(optionNodes[2].value, 'bar');
+        t.equal(optionNodes[3].value, 'baz');
     }));
 
     s.test('renders the empty item', sync(function (t) {
@@ -314,16 +403,43 @@ suite('With ampersand collection', function (s) {
 
         var optionNodes = view.el.querySelectorAll('select option');
 
-        t.equal(optionNodes.length, 4);
+        t.equal(optionNodes.length, 5);
 
         t.equal(optionNodes[0].value, '');
         t.equal(optionNodes[0].innerHTML, 'Please choose:');
 
-        t.equal(optionNodes[1].value, '1');
-        t.equal(optionNodes[1].textContent, 'Option one');
+        t.equal(optionNodes[2].value, '1');
+        t.equal(optionNodes[2].textContent, 'Option one');
     }));
 
-    s.test('selects the right item by id/model', sync(function (t) {
+    s.test('exhibits normal behavior on null model option', sync(function (t) {
+        var coll = new VolatileCollection([
+            { id: 0, someOtherKey: 'foo', title: 'Option zero' },
+            { id: null, someOtherKey: 'bar', title: 'Option null' },
+            { id: 2, someOtherKey: 'baz', title: 'Option two' },
+        ]);
+        view = new SelectView({
+            name: 'testNullId',
+            options: coll,
+            idAttribute: 'id',
+            textAttribute: 'title'
+        });
+
+        var optionNodes = view.el.querySelectorAll('select option');
+
+        t.equal(optionNodes.length, 3);
+
+        t.equal(optionNodes[0].value, '0', 'option before null model has correct value');
+        t.equal(optionNodes[0].textContent, 'Option zero');
+
+        t.equal(view.value, null); // null option set by default when no value provided
+        t.equal(optionNodes[1].value, '');
+
+        t.equal(optionNodes[2].value, '2', 'option after null model has correct value');
+        t.equal(optionNodes[2].textContent, 'Option two');
+    }));
+
+    s.test('selects the right item by id/model (options: collection)', sync(function (t) {
         view = new SelectView({
             name: 'word',
             options: coll,
@@ -336,32 +452,27 @@ suite('With ampersand collection', function (s) {
 
         var select = view.el.querySelector('select');
 
-        t.equal(view.value, coll.at(1));
+        t.equal(view.value, coll.at(2));
         t.ok(view.valid);
         t.equal(select.options[select.selectedIndex].innerHTML, 'Option two');
 
-        view.setValue(undefined);
-        t.equal(view.value, undefined);
+        view.clear();
+        t.equal(view.value, null);
         t.notOk(view.valid);
         t.equal(select.options[select.selectedIndex].innerHTML, 'Please choose:');
 
         view.setValue('1');
-        t.equal(view.value, coll.at(0));
+        t.equal(view.value, coll.at(1));
         t.ok(view.valid);
         t.equal(select.options[select.selectedIndex].innerHTML, 'Option one');
 
-        view.setValue('totes-wrong');
-        t.equal(view.value, undefined);
-        t.notOk(view.valid);
-        t.equal(select.options[select.selectedIndex].innerHTML, 'Please choose:');
-
-        view.setValue(coll.at(1));
-        t.equal(view.value, coll.at(1));
+        view.setValue(coll.at(2));
+        t.equal(view.value, coll.at(2));
         t.ok(view.valid);
         t.equal(select.options[select.selectedIndex].innerHTML, 'Option two');
     }));
 
-    s.test('selects the right item by id/model, with yieldModel: false', sync(function (t) {
+    s.test('selects the right item by id/model (options: collection), with yieldModel: false', sync(function (t) {
         view = new SelectView({
             name: 'word',
             options: coll,
@@ -380,7 +491,7 @@ suite('With ampersand collection', function (s) {
         t.equal(select.options[select.selectedIndex].innerHTML, 'Option two');
 
         view.setValue(undefined);
-        t.equal(view.value, void 0);
+        t.equal(view.value, null);
         t.notOk(view.valid);
         t.equal(select.options[select.selectedIndex].innerHTML, 'Please choose:');
 
@@ -389,15 +500,17 @@ suite('With ampersand collection', function (s) {
         t.ok(view.valid);
         t.equal(select.options[select.selectedIndex].innerHTML, 'Option one');
 
-        view.setValue('totes-wrong');
-        t.equal(view.value, void 0);
-        t.notOk(view.valid);
-        t.equal(select.options[select.selectedIndex].innerHTML, 'Please choose:');
-
-        view.setValue(coll.at(1));
+        view.setValue(coll.get(2));
         t.equal(view.value, 2);
         t.ok(view.valid);
         t.equal(select.options[select.selectedIndex].innerHTML, 'Option two');
+
+        try {
+            view.setValue(1000);
+            t.ok(false, 'should not be able to set invalid model id or model obj');
+        } catch (err) {
+            t.ok(true, 'should not be able to set invalid model id or model obj');
+        }
     }));
 
     s.test('renders a disabled item for a model that has the attribute specified in the disabledAttribute option set to truthy', sync(function (t) {
@@ -424,4 +537,5 @@ suite('With ampersand collection', function (s) {
         t.equal(optionNodes[3].disabled, false);
         t.equal(optionNodes[4].disabled, true);
     }));
+
 });

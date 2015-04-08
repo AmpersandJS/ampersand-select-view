74e320d7e518fddc7450888f0c30ac840c25c124
Alex Potsides <alex@achingbrain.net>
2015-04-08 10:29:06 +0100

    Check for parentNode before invoking removeChild
    Brings ampersand-select-view more in line with how [ampersand-view does it](https://github.com/AmpersandJS/ampersand-view/blob/21403d2a38771dd709b4282731637c6b19fc4175/ampersand-view.js#L148), otherwise you occasionally get `Uncaught TypeError: Cannot read property 'removeChild' of null`.

--- ampersand-select-view.js
+++ ampersand-select-view.js
@@ -175,7 +175,7 @@ SelectView.prototype.updateSelectedOption = function () {
 };
 
 SelectView.prototype.remove = function () {
-    if (this.el) this.el.parentNode.removeChild(this.el);
+    if (this.el && this.el.parentNode) this.el.parentNode.removeChild(this.el);
     this.el.removeEventListener('change', this.onChange, false);
 };
 
